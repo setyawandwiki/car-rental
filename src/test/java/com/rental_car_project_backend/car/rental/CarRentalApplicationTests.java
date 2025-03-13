db3b22094f7d1063f0f7ce0da6b7b590f4f27784
setyawandwiki <setyawandwiki1@gmail.com>
2025-03-13 22:04:54 +0700

    first commit

---
+++ src/test/java/com/rental_car_project_backend/car/rental/CarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental_car_project_backend.car.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
