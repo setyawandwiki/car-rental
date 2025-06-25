4629674327cf3e70f1b9fbf0a3fa4e9d7a94f08b
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-25 17:36:54 +0700

    feat(db) : insert genders data

--- src/main/resources/db/migrations/V1__init_schema.sql
+++ src/main/resources/db/migrations/V1__init_schema.sql
@@ -1,33 +1,40 @@
 CREATE TABLE countries (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE cities (
     id BIGSERIAL PRIMARY KEY,
     id_country INT,
     name VARCHAR(255),
-    created_at DATE,
-    updated_at DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_country FOREIGN KEY (id_country) REFERENCES countries(id)
 );
 
 CREATE TABLE genders (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE address (
     id BIGSERIAL PRIMARY KEY,
     address VARCHAR(255),
     id_city INT,
-    created_at DATE,
-    updated_at DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_city FOREIGN KEY (id_city) REFERENCES cities(id)
+);
+
+CREATE TABLE roles(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE users (
@@ -40,24 +47,26 @@ CREATE TABLE users (
     password VARCHAR(255),
     id_gender INT,
     id_address INT,
-    created_at DATE,
-    updated_at DATE,
+    id_role INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_gender FOREIGN KEY (id_gender) REFERENCES genders(id),
-    CONSTRAINT fk_address FOREIGN KEY (id_address) REFERENCES address(id)
+    CONSTRAINT fk_address FOREIGN KEY (id_address) REFERENCES address(id),
+    CONSTRAINT fk_roles FOREIGN KEY (id_role) REFERENCES roles(id)
 );
 
 CREATE TABLE car_types (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE status (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE cars (
@@ -67,8 +76,8 @@ CREATE TABLE cars (
     baggages INT,
     year INT, -- ubah kolom 'year' yang tadinya pakai tanda kutip!
     image VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE companies (
@@ -76,8 +85,8 @@ CREATE TABLE companies (
     name VARCHAR(255),
     rate INT,
     image VARCHAR(255),
-    created_at DATE,
-    updated_at DATE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE company_cars (
@@ -86,8 +95,8 @@ CREATE TABLE company_cars (
     id_car INT,
     price INT,
     id_car_type INT,
-    created_at DATE,
-    updated_at DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_car_types FOREIGN KEY (id_car_type) REFERENCES car_types (id),
     CONSTRAINT fk_car FOREIGN KEY (id_car) REFERENCES cars(id),
     CONSTRAINT fk_company FOREIGN KEY (id_company) REFERENCES companies(id)
@@ -103,8 +112,8 @@ CREATE TABLE orders (
     id_status INT,
     pickup_date DATE,
     dropoff_date DATE,
-    created_at DATE,
-    updated_at DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES users(id),
     CONSTRAINT fk_company FOREIGN KEY (id_company) REFERENCES companies(id),
     CONSTRAINT fk_status FOREIGN KEY (id_status) REFERENCES status(id)
