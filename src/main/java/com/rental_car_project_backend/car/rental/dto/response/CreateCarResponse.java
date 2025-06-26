49f9c2efaae98d60357b8dc8d262350444e57516
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 20:21:16 +0700

    feat(controller) : adding car controller

---
+++ src/main/java/com/rental_car_project_backend/car/rental/dto/response/CreateCarResponse.java
@@ -0,0 +1,26 @@
+package com.rental_car_project_backend.car.rental.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CreateCarResponse {
+    private Integer id;
+    private String name;
+    private Integer seats;
+    private Integer baggages;
+    private Integer year;
+    private String image;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
