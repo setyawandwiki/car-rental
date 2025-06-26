b00d62c2d7918cc7bf47455dec496b4153ef15e6
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 13:00:10 +0700

    feat(config) : adding custom user deteail service

---
+++ src/main/java/com/rental_car_project_backend/car/rental/config/CustomUserDetailService.java
@@ -0,0 +1,12 @@
+package com.rental_car_project_backend.car.rental.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
