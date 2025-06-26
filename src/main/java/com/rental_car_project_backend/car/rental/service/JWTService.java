08d60d7baa59079fe6ecba6b4a0e649c346392cb
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 15:18:35 +0700

    feat(exceptions) : add username and password invalid exception

--- src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
@@ -1,5 +1,6 @@
 package com.rental_car_project_backend.car.rental.service;
 
+import com.rental_car_project_backend.car.rental.dto.response.AuthResponse;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class JWTService {
@@ -9,5 +10,9 @@ public class JWTService {
 
     public boolean validateToken(String token, UserDetails userDetails) {
         return false;
+    }
+
+    public AuthResponse generateToken(String email) {
+        return null;
     }
 }
--- src/main/java/com/rental_car_project_backend/car/rental/service/impl/AuthServiceImpl.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/impl/AuthServiceImpl.java
@@ -3,11 +3,18 @@ package com.rental_car_project_backend.car.rental.service.impl;
 import com.rental_car_project_backend.car.rental.dto.request.AuthRequest;
 import com.rental_car_project_backend.car.rental.dto.request.UserRequest;
 import com.rental_car_project_backend.car.rental.dto.response.AuthResponse;
+import com.rental_car_project_backend.car.rental.dto.response.UserResponse;
+import com.rental_car_project_backend.car.rental.entity.Users;
 import com.rental_car_project_backend.car.rental.repository.UserRepository;
 import com.rental_car_project_backend.car.rental.service.AuthService;
+import com.rental_car_project_backend.car.rental.service.JWTService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,13 +23,44 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private final JWTService jwtService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
     @Override
     public AuthResponse register(UserRequest request) {
-        return null;
+        Users users = new Users();
+        users.setBirthDate(request.getBirthDate());
+        users.setEmail(request.getEmail());
+        users.setIdCity(request.getIdCity());
+        users.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
+        users.setIdRole(request.getIdRole());
+        users.setFullName(request.getFullName());
+        users.setIdCity(request.getIdCity());
+        Users save = userRepository.save(users);
+        UserResponse userResponse = UserResponse.builder()
+                .id(save.getId())
+                .email(save.getEmail())
+                .birthDate(save.getBirthDate())
+                .fullName(save.getFullName())
+                .idCity(save.getIdCity())
+                .phoneNumber(save.getPhoneNumber())
+                .build();
+        return AuthResponse.builder()
+                .message("User registeres sucessfully")
+                .userResponse(userResponse)
+                .build();
     }
 
     @Override
     public AuthResponse login(AuthRequest request) {
-        return null;
+        Authentication authenticate = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        if(authenticate.isAuthenticated()){
+            return jwtService.generateToken(request.getEmail());
+        }else{
+            throw UsernameAndPasswordInvalidException();
+        }
     }
 }
