08250a835d67bcff83a628e3c539fb06d90331cf
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 13:38:36 +0700

    feat(service) : adding authservice

---
+++ src/main/java/com/rental_car_project_backend/car/rental/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.rental_car_project_backend.car.rental.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AuthService {
+
+}
--- src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
@@ -1,4 +1,13 @@
 package com.rental_car_project_backend.car.rental.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class JWTService {
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return false;
+    }
 }
