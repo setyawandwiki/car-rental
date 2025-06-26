0e74f91151495ead513b08ad22401c7d98963525
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 16:21:15 +0700

    chore(pom.xml) menambahkan jjwt-api jjwt-impl jjwt-jackson dependencies

--- src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
@@ -1,9 +1,19 @@
 package com.rental_car_project_backend.car.rental.service;
 
 import com.rental_car_project_backend.car.rental.dto.response.AuthResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+
+@Service
 public class JWTService {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    public SecretKey secretKey(){
+//        byte[] keyBytes =
+    }
     public String extractUsername(String token) {
         return null;
     }
@@ -12,7 +22,7 @@ public class JWTService {
         return false;
     }
 
-    public AuthResponse generateToken(String email) {
+    public String generateToken(String email) {
         return null;
     }
 }
