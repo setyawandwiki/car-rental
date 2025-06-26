403d0be472ec8a1559a24bcc372469f579a33d19
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 13:44:37 +0700

    feat(impl) : adding service impl

--- src/main/java/com/rental_car_project_backend/car/rental/dto/AuthResponse.java
+++
@@ -1,7 +0,0 @@
-package com.rental_car_project_backend.car.rental.dto;
-
-public class AuthResponse {
-    private String message;
-    private String token;
-    private
-}
---
+++ src/main/java/com/rental_car_project_backend/car/rental/dto/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.rental_car_project_backend.car.rental.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+    private String message;
+    private String token;
+    private UserResponse userResponse;
+}
