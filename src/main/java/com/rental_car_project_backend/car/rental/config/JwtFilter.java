c707edf1ec84a8683502e19d8de97accd5b26641
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 13:29:29 +0700

    feat(service) : add jwt service file

--- src/main/java/com/rental_car_project_backend/car/rental/config/JwtFilter.java
+++ src/main/java/com/rental_car_project_backend/car/rental/config/JwtFilter.java
@@ -1,4 +1,29 @@
 package com.rental_car_project_backend.car.rental.config;
 
-public class JwtFilter {
+import com.rental_car_project_backend.car.rental.service.JWTService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JWTService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.isAuthenticated()){
+            new UsernamePasswordAuthenticationToken(request.pas)
+        }
+    }
 }
---
+++ src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
@@ -0,0 +1,4 @@
+package com.rental_car_project_backend.car.rental.service;
+
+public class JWTService {
+}
