661b498956f53e391c9f61c2cf88faf9d45966a7
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 16:34:31 +0700

    feat(service): add method with generate token method and validation

--- src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/JWTService.java
@@ -1,28 +1,68 @@
 package com.rental_car_project_backend.car.rental.service;
 
-import com.rental_car_project_backend.car.rental.dto.response.AuthResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.io.Decoders;
 
 import javax.crypto.SecretKey;
+import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
+
     @Value("${jwt.secret}")
     private String jwtSecret;
+
     public SecretKey secretKey(){
-//        byte[] keyBytes =
+        byte[] decode = Decoders.BASE64URL.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(decode);
     }
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
-        return false;
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpirationToken(token).before(new Date());
+    }
+
+    private Date extractExpirationToken(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     public String generateToken(String email) {
-        return null;
+        Map<String, Objects> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .and()
+                .signWith(secretKey())
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 60 * 30))
+                .compact();
     }
 }
