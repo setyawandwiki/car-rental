35915f26b17ba551b25eee3372774f47ea7fee62
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-26 14:39:25 +0700

    feat(roles) : add roles entity

--- src/main/java/com/rental_car_project_backend/car/rental/dto/response/UserResponse.java
+++ src/main/java/com/rental_car_project_backend/car/rental/dto/response/UserResponse.java
@@ -1,13 +1,24 @@
 package com.rental_car_project_backend.car.rental.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserResponse {
+    private int id;
+    private String fullName;
+    private int idCity;
+    private Date birthDate;
+    private String email;
+    private String phoneNumber;
 }
