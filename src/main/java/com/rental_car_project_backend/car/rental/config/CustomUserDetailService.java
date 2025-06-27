d49284a0b38a02e83d03bc609f09959de55eb33c
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-27 17:43:19 +0700

    fix(config) : using hasuthority for fixing bugs on filterchain

--- src/main/java/com/rental_car_project_backend/car/rental/config/CustomUserDetailService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/config/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.rental_car_project_backend.car.rental.config;
 import com.rental_car_project_backend.car.rental.entity.UserPrincipal;
 import com.rental_car_project_backend.car.rental.entity.Users;
 import com.rental_car_project_backend.car.rental.exceptions.UserNotFoundException;
+import com.rental_car_project_backend.car.rental.repository.RoleRepository;
 import com.rental_car_project_backend.car.rental.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +12,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Component
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private final RoleRepository roleRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         Users users = userRepository.findByEmail(username).orElseThrow(() ->{
-            System.out.println(username);
           throw new UserNotFoundException("User not found with email " + username);
         });
-
-        return new UserPrincipal(users);
+        String name = roleRepository.findById(users.getIdRole()).orElseThrow(() ->
+                new RuntimeException("role not found")).getName();
+        return new UserPrincipal(users, name);
     }
 }
