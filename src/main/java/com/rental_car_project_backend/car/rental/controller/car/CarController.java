56d1b40d7368091319a835f53c98ad2f7417d214
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-28 19:39:59 +0700

    feat(exceptions) : adding carNotFoundExceptions

--- src/main/java/com/rental_car_project_backend/car/rental/controller/car/CarController.java
+++ src/main/java/com/rental_car_project_backend/car/rental/controller/car/CarController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/car")
@@ -19,6 +16,11 @@ public class CarController {
     private final CarService carService;
     @PostMapping
     public ResponseEntity<CreateCarResponse> createCar(@Valid @RequestBody CreateCarRequest request){
+        CreateCarResponse createCarResponse = carService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createCarResponse);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CreateCarResponse> createCar(@PathVariable("id") Integer id){
         CreateCarResponse createCarResponse = carService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createCarResponse);
     }
