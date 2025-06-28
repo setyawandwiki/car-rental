821914d07de0bf2ed6c07bdb763a3b636f308613
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-28 21:17:09 +0700

    feat(service) : adding updatecar method

--- src/main/java/com/rental_car_project_backend/car/rental/service/impl/CarService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/impl/CarService.java
@@ -1,9 +1,11 @@
 package com.rental_car_project_backend.car.rental.service.impl;
 
 import com.rental_car_project_backend.car.rental.dto.request.CreateCarRequest;
+import com.rental_car_project_backend.car.rental.dto.request.UpdateCarRequest;
 import com.rental_car_project_backend.car.rental.dto.response.CreateCarResponse;
 import com.rental_car_project_backend.car.rental.dto.response.DeleteCarResponse;
 import com.rental_car_project_backend.car.rental.dto.response.GetCarResponse;
+import com.rental_car_project_backend.car.rental.dto.response.UpdateCarResponse;
 
 import java.util.List;
 
@@ -12,4 +14,5 @@ public interface CarService {
     GetCarResponse getCar(Integer id);
     List<GetCarResponse> getCars();
     DeleteCarResponse deleteCarById(Integer id);
+    UpdateCarResponse updateCar(Integer id, UpdateCarRequest request);
 }
