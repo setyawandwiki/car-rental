a23a925d95fb4ba9c9e588d95aa8564de6ec2b20
setyawandwiki <setyawandwiki1@gmail.com>
2025-06-30 15:47:29 +0700

    feat(controoller) : adding udpate company controller

--- src/main/java/com/rental_car_project_backend/car/rental/controller/CompanyController.java
+++ src/main/java/com/rental_car_project_backend/car/rental/controller/CompanyController.java
@@ -1,16 +1,15 @@
 package com.rental_car_project_backend.car.rental.controller;
 
 import com.rental_car_project_backend.car.rental.dto.request.CreateCompanyRequest;
+import com.rental_car_project_backend.car.rental.dto.request.UpdateCompanyRequest;
 import com.rental_car_project_backend.car.rental.dto.response.CreateCompanyResponse;
+import com.rental_car_project_backend.car.rental.dto.response.UpdateCompanyResponse;
 import com.rental_car_project_backend.car.rental.service.CompanyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -27,5 +26,19 @@ public class CompanyController {
         CreateCompanyRequest request = new CreateCompanyRequest(name, rate, image);
         CreateCompanyResponse company = companyService.createCompany(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(company);
+    }
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<UpdateCompanyResponse> updateCompany(@PathVariable("id") Integer id,
+                                                               @Valid @RequestParam(value = "name", required = false)
+                                                               String name,
+                                                               @RequestParam(value = "image", required = false)
+                                                                   MultipartFile image,
+                                                               @RequestParam(value = "rate", required = false)
+                                                                   Double rate
+    )
+            throws IOException {
+        UpdateCompanyRequest request = new UpdateCompanyRequest(name, rate, image);
+        UpdateCompanyResponse updateCompanyResponse = companyService.updateCompanyResponse(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(updateCompanyResponse);
     }
 }
