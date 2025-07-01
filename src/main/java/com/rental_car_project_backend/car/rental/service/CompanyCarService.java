28a8769eaee1ae8676d19d2d4ac8cf73ab010064
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-01 15:17:20 +0700

    feat(service) : adding get all company car implementation

--- src/main/java/com/rental_car_project_backend/car/rental/service/CompanyCarService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/CompanyCarService.java
@@ -3,7 +3,11 @@ package com.rental_car_project_backend.car.rental.service;
 import com.rental_car_project_backend.car.rental.dto.request.CreateCompanyCarRequest;
 import com.rental_car_project_backend.car.rental.dto.request.CreateCompanyRequest;
 import com.rental_car_project_backend.car.rental.dto.response.CreateCompanyCarResponse;
+import com.rental_car_project_backend.car.rental.dto.response.GetCompanyCarResponse;
+
+import java.util.List;
 
 public interface CompanyCarService {
     CreateCompanyCarResponse createCompanyCar(CreateCompanyCarRequest request);
+    List<GetCompanyCarResponse> getCompanyCars();
 }
