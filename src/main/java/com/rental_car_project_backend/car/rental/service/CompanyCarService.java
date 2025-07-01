4c5cebcc47811d47e3a202ef50e9802684ef1008
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-01 14:50:32 +0700

    feat(service) : create company car service

--- src/main/java/com/rental_car_project_backend/car/rental/service/CompanyCarService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/CompanyCarService.java
@@ -1,4 +1,8 @@
 package com.rental_car_project_backend.car.rental.service;
 
+import com.rental_car_project_backend.car.rental.dto.request.CreateCompanyRequest;
+import com.rental_car_project_backend.car.rental.dto.response.CreateCompanyCarResponse;
+
 public interface CompanyCarService {
+    CreateCompanyCarResponse createCompanyCar(CreateCompanyRequest request);
 }
