a89bbdba28913d463b0a87f735e5fdff583b7bc9
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-01 14:56:22 +0700

    feat(config) : adding requestmatchers for company car on securityconfig

--- src/main/java/com/rental_car_project_backend/car/rental/service/impl/CompanyCarServiceImpl.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/impl/CompanyCarServiceImpl.java
@@ -2,15 +2,22 @@ package com.rental_car_project_backend.car.rental.service.impl;
 
 import com.rental_car_project_backend.car.rental.dto.request.CreateCompanyRequest;
 import com.rental_car_project_backend.car.rental.dto.response.CreateCompanyCarResponse;
+import com.rental_car_project_backend.car.rental.repository.CompanyCarRepository;
 import com.rental_car_project_backend.car.rental.service.CompanyCarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyCarServiceImpl implements CompanyCarService {
+    private final CompanyCarRepository companyCarRepository;
     @Override
     public CreateCompanyCarResponse createCompanyCar(CreateCompanyRequest request) {
+        boolean authenticated = SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+        if(!authenticated){
+            throw new SecurityException("You must logged in first!");
+        }
         return null;
     }
 }
