6a2d5e8a66d8e02caf017ee49396313707cd6976
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-05 17:10:07 +0700

    feat(exceptions) : add payment exceptions

---
+++ src/main/java/com/rental_car_project_backend/car/rental/exceptions/PaymentExceptions.java
@@ -0,0 +1,7 @@
+package com.rental_car_project_backend.car.rental.exceptions;
+
+public class PaymentExceptions extends RuntimeException {
+    public PaymentExceptions(String message) {
+        super(message);
+    }
+}
