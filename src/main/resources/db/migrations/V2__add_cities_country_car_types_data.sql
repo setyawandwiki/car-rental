15918633ab2ba1e8d764d171a971bfcca1102824
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-05 13:17:44 +0700

    feat(controller): add order controller

--- src/main/resources/db/migrations/V2__add_cities_country_car_types_data.sql
+++ src/main/resources/db/migrations/V2__add_cities_country_car_types_data.sql
@@ -3,4 +3,6 @@ INSERT INTO countries(name) VALUES ('Indonesia');
 INSERT INTO cities (id_country, name) VALUES (1, 'Bogor'),
 (1, 'Jakarta'),
 (1, 'Cibinong'),
-(1, 'Cibubur')
+(1, 'Cibubur');
+
+INSERT INTO car_types (name) VALUES ('Automatic'), ('Manual');
