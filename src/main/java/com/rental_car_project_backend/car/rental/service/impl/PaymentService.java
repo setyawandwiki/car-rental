fa40317dd006ab7fc5a980d9b0da0519a591cf82
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-05 17:13:00 +0700

    feat(service) : add payment service

--- src/main/java/com/rental_car_project_backend/car/rental/service/impl/PaymentService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/impl/PaymentService.java
@@ -1,4 +1,10 @@
 package com.rental_car_project_backend.car.rental.service.impl;
 
-public class PaymentService {
+import com.rental_car_project_backend.car.rental.dto.request.CreatePaymentRequest;
+import com.rental_car_project_backend.car.rental.dto.request.PaymentNotificationRequest;
+import com.rental_car_project_backend.car.rental.dto.response.CreatedPaymentResponse;
+
+public interface PaymentService {
+    CreatedPaymentResponse createPayment(CreatePaymentRequest request);
+    void handlePaymentNotification(PaymentNotificationRequest request);
 }
