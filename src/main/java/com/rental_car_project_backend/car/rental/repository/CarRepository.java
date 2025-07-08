878742ae7f44fff398ab89801d5f7b75c43f27c0
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-08 19:29:32 +0700

    feat(repository) : add jpaspecification executor on car repo

--- src/main/java/com/rental_car_project_backend/car/rental/repository/CarRepository.java
+++ src/main/java/com/rental_car_project_backend/car/rental/repository/CarRepository.java
@@ -2,6 +2,7 @@ package com.rental_car_project_backend.car.rental.repository;
 
 import com.rental_car_project_backend.car.rental.entity.Cars;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CarRepository extends JpaRepository<Cars, Integer> {
+public interface CarRepository extends JpaRepository<Cars, Integer>, JpaSpecificationExecutor<Cars> {
 }
