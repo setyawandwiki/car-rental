534f0881e5b6d9c307169ecdd5abbbb83e889679
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-09 11:33:01 +0700

    feat(service): add user service

--- src/main/java/com/rental_car_project_backend/car/rental/service/UserService.java
+++ src/main/java/com/rental_car_project_backend/car/rental/service/UserService.java
@@ -1,9 +1,10 @@
 package com.rental_car_project_backend.car.rental.service;
 
 import com.rental_car_project_backend.car.rental.dto.request.page.PageRequestDTO;
+import com.rental_car_project_backend.car.rental.dto.request.page.SearchUserDTO;
 import com.rental_car_project_backend.car.rental.dto.response.user.UserResponse;
 import org.springframework.data.domain.Page;
 
 public interface UserService {
-    Page<UserResponse> getAllUsers(PageRequestDTO pageRequestDTO, );
+    Page<UserResponse> getAllUsers(PageRequestDTO pageRequestDTO, SearchUserDTO searchUserDTO);
 }
