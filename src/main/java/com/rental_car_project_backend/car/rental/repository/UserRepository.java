54dd2d034be6f69786a689eb17f2c8259abf8a96
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-09 11:24:54 +0700

    feat(repository) : add jpaspecification executor for user repository

--- src/main/java/com/rental_car_project_backend/car/rental/repository/UserRepository.java
+++ src/main/java/com/rental_car_project_backend/car/rental/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.rental_car_project_backend.car.rental.repository;
 
 import com.rental_car_project_backend.car.rental.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Users, Integer> {
+public interface UserRepository extends JpaRepository<Users, Integer>, JpaSpecificationExecutor<Users> {
     @Query(value = """
             SELECT u.* FROM users u WHERE u.email = :email 
             """, nativeQuery = true)
