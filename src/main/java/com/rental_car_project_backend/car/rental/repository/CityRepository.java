d8c2c85e6e0312b0a1d6602be58ac370e8ab20a1
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-28 20:46:16 +0700

    feat(dto) : adding getcitiesresponse

--- src/main/java/com/rental_car_project_backend/car/rental/repository/CityRepository.java
+++ src/main/java/com/rental_car_project_backend/car/rental/repository/CityRepository.java
@@ -2,6 +2,7 @@ package com.rental_car_project_backend.car.rental.repository;
 
 import com.rental_car_project_backend.car.rental.entity.Cities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CityRepository extends JpaRepository<Cities, Integer> {
+public interface CityRepository extends JpaRepository<Cities, Integer>, JpaSpecificationExecutor<Cities> {
 }
