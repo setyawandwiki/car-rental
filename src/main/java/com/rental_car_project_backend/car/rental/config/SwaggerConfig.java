1e33859d12bc80d39dbb76ffcf01a01295cb607e
setyawandwiki <setyawandwiki1@gmail.com>
2025-08-04 17:42:05 +0700

    feat(config) : adding swagger configuration

---
+++ src/main/java/com/rental_car_project_backend/car/rental/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.rental_car_project_backend.car.rental.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")))
+                /*untuk identifikasi data jwtnya, seperti title dsb*/
+                .info(new Info()
+                        .title("Car Rental")
+                        .version("1.0")
+                        .description("Car Rental Application back end"));
+    }
+}
