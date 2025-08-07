deb7938fc8808dd62ca2937413e7813f9f399075
setyawandwiki <setyawandwiki1@gmail.com>
2025-08-07 10:10:25 +0700

    feat(test impl) : adding car service impl test file

---
+++ src/test/java/com/rental_car_project_backend/car/rental/service/impl/CarServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.rental_car_project_backend.car.rental.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceImplTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getCar() {
+    }
+
+    @Test
+    void getCars() {
+    }
+
+    @Test
+    void deleteCarById() {
+    }
+
+    @Test
+    void updateCar() {
+    }
+}
