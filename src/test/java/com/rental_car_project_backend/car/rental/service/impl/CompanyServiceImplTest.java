e7d7e50ff8d157a56ffc785c40e959894c26f209
setyawandwiki <setyawandwiki1@gmail.com>
2025-08-09 16:44:10 +0700

    feat(test company) : adding companyServiceImpl_CreateCompanyMethodSucceeded

--- src/test/java/com/rental_car_project_backend/car/rental/service/impl/CompanyServiceImplTest.java
+++ src/test/java/com/rental_car_project_backend/car/rental/service/impl/CompanyServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.rental_car_project_backend.car.rental.service.impl;
 
 import com.rental_car_project_backend.car.rental.dto.request.company.CreateCompanyRequest;
+import com.rental_car_project_backend.car.rental.entity.Companies;
 import com.rental_car_project_backend.car.rental.entity.Users;
+import com.rental_car_project_backend.car.rental.entity.Vendor;
 import com.rental_car_project_backend.car.rental.exceptions.IncompletableDataException;
 import com.rental_car_project_backend.car.rental.exceptions.UserNotFoundException;
 import com.rental_car_project_backend.car.rental.repository.CityRepository;
@@ -14,15 +16,15 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,6 +43,10 @@ class CompanyServiceImplTest {
     private CityRepository cityRepository;
     @InjectMocks
     private CompanyServiceImpl companyService;
+    @Captor
+    ArgumentCaptor<Companies> companiesArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Vendor> vendorArgumentCaptor;
     CreateCompanyRequest request;
     @BeforeEach
     public void setUp(){
@@ -95,7 +101,6 @@ class CompanyServiceImplTest {
                 .id(1)
                 .email("test@gmail.com")
                 .build();
-        Users spy = Mockito.spy(users);
         Authentication fakeAuthentication = Mockito.mock(Authentication.class);
         SecurityContext fakeContext = Mockito.mock(SecurityContext.class);
         SecurityContextHolder.setContext(fakeContext);
@@ -110,6 +115,56 @@ class CompanyServiceImplTest {
                 .isInstanceOf(IncompletableDataException.class)
                 .hasMessageContaining("User data is incomplete: " +
                         "full name, bank code, or account number is missing");
+    }
+
+    @Test
+    void companyServiceImpl_CreateCompanyMethodSucceeded() throws IOException {
+        // given
+        Users users = Users.builder()
+                .id(1)
+                .email("test@gmail.com")
+                .fullName("dwiki setyawan")
+                .bankCode("BCA")
+                .accountNumber("123456789")
+                .build();
+        Companies companies = new Companies();
+        companies.setCreatedAt(LocalDateTime.now());
+        companies.setRate(request.getRate());
+        companies.setName(request.getName());
+        companies.setIdUser(users.getId());
+        companies.setIdCity(request.getIdCity());
+        companies.setImage(request.getImageFile().toString());
+        Vendor vendor = Vendor.builder()
+                .id(1)
+                .companyId(1)
+                .availableBalance(1000.0)
+                .companyId(1)
+                .build();
+        Authentication fakeAuthentication = Mockito.mock(Authentication.class);
+        SecurityContext fakeContext = Mockito.mock(SecurityContext.class);
+        SecurityContextHolder.setContext(fakeContext);
+        Mockito.when(fakeContext.getAuthentication()).thenReturn(fakeAuthentication);
+        Mockito.when(fakeAuthentication.getName()).thenReturn("test@gmail.com");
+        Mockito.when(fakeAuthentication.isAuthenticated()).thenReturn(true);
+        Mockito.when(userRepository.findByEmail("test@gmail.com")).thenReturn(Optional.of(users));
+        Mockito.when(imageUploadService.uploadImage(Mockito.eq
+                (request.getImageFile()), Mockito.anyString(), Mockito.eq("company-images")))
+                        .thenReturn("image-upload");
+        Mockito.when(companyRepository.save(Mockito.any(Companies.class))).thenReturn(companies);
+        Mockito.when(vendorRepository.save(Mockito.any(Vendor.class))).thenReturn(vendor);
+        // when
+        companyService.createCompany(request);
+        // then
+        Mockito.verify(companyRepository).save(companiesArgumentCaptor.capture());
+        Mockito.verify(vendorRepository).save(vendorArgumentCaptor.capture());
+
+        Companies value = companiesArgumentCaptor.getValue();
+        Vendor vendorCapture = vendorArgumentCaptor.getValue();
+        assertThat(value.getName()).isEqualTo("BMW");
+        /**
+         * nilai vendornya diambil dari implnya
+         * */
+        assertThat(vendorCapture.getCompanyId()).isEqualTo(value.getId());
     }
 
     @Test
