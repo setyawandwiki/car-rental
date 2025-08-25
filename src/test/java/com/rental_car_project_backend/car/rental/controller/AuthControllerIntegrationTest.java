a1bdea9e03a6b5421959287386d5291e162e3537
setyawandwiki <setyawandwiki1@gmail.com>
2025-08-25 21:18:37 +0700

    feat(integration test authcontroller) : adding login_WithValidData_ShouldSucceed

--- src/test/java/com/rental_car_project_backend/car/rental/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/rental_car_project_backend/car/rental/controller/AuthControllerIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.rental_car_project_backend.car.rental.controller;
 
+import com.rental_car_project_backend.car.rental.dto.request.user.LoginRequest;
 import com.rental_car_project_backend.car.rental.dto.request.user.RegisterRequest;
+import com.rental_car_project_backend.car.rental.dto.response.user.LoginResponse;
 import com.rental_car_project_backend.car.rental.dto.response.user.RegisterResponse;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,12 +43,27 @@ class AuthControllerIntegrationTest {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
     }
 
-    @Test
-    void register_ShouldRegister() {
-        // given
+    @BeforeEach
+    void setupUser() {
         RegisterRequest request = RegisterRequest.builder()
                 .password("dwiki123")
                 .email("setyawandwiki1@gmail.com")
+                .build();
+
+        testRestTemplate.exchange(
+                "/auth/register",
+                HttpMethod.POST,
+                new HttpEntity<>(request),
+                RegisterResponse.class
+        );
+    }
+
+    @Test
+    void register_WithValidData_ShouldSucceed() {
+        // given
+        RegisterRequest request = RegisterRequest.builder()
+                .password("dwiki123")
+                .email("setyawandwiki2@gmail.com")
                 .build();
 
         // when
@@ -56,13 +74,27 @@ class AuthControllerIntegrationTest {
                 RegisterResponse.class
         );
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getEmail()).isEqualTo("setyawandwiki1@gmail.com");
+        assertThat(response.getBody().getEmail()).isEqualTo("setyawandwiki2@gmail.com");
     }
 
     @Test
-    void login() {
+    void login_WithValidData_ShouldSucceed() {
+        LoginRequest loginRequest = LoginRequest.builder()
+                .password("dwiki123")
+                .email("setyawandwiki1@gmail.com")
+                .build();
+
+        ResponseEntity<LoginResponse> loginResponse = testRestTemplate.exchange(
+                "/auth/login",
+                HttpMethod.POST,
+                new HttpEntity<>(loginRequest),
+                LoginResponse.class
+        );
+
+        assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(loginResponse.getBody()).isNotNull();
+        assertThat(loginResponse.getBody().getMessage()).isEqualTo("Login Successful");
     }
 }
